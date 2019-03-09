a69e087cbb6c11b18fb76db8c4c964a5c06e00b8
kyruski <jamescboyett@gmail.com>
2019-03-08 19:25:39 -0600

    Sets up React Router skeleton

--- challenge_1/client/Search.jsx
+++ challenge_1/client/Search.jsx
@@ -3,22 +3,30 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const Search = function(props) {
   return (
-    <div style={{ border: "1px black solid" }}>
-      What topic would you like to search for?
-      <form>
-        <input
-          type="text"
-          id="search-text"
-          style={{
-            width: "700px",
-            height: "30px",
-            borderRadius: "5px",
-            fontSize: "20px"
-          }}
-        />
-        <button type="button">Search</button>
-      </form>
-    </div>
+    <Router>
+      <div style={{ border: "1px black solid" }}>
+        What topic would you like to search for?
+        <br />
+        <Link to="/Date">By Date</Link> - <Link to="/Place">By Place</Link> -{" "}
+        <Link to="/Topic">By Topic/Description</Link>
+        <Route path="/Date" component={() => <SearchDate />} />
+        <Route path="/Place" component={() => <SearchPlace />} />
+        <Route path="/Topic" component={() => <SearchTopic />} />
+        <form>
+          <input
+            type="text"
+            id="search-text"
+            style={{
+              width: "700px",
+              height: "30px",
+              borderRadius: "5px",
+              fontSize: "20px"
+            }}
+          />
+          <button type="button">Search</button>
+        </form>
+      </div>
+    </Router>
   );
 };
 
