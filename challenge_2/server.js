88e443198849450c7ebbc921aeb165659340eb16
kyruski <jamescboyett@gmail.com>
2019-03-11 10:57:52 -0500

    Adds the server skeleton

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const port = 3000;
 
+const app = express();
 
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+
+app.listen(port, () => console.log('Server running at port 3000'));
