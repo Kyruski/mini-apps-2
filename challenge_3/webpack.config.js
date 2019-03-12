572d02d88c500ccc890a38e7d1b10125bba4673c
kyruski <jamescboyett@gmail.com>
2019-03-12 10:09:16 -0500

    Initial commit, server setup and webpack setup

---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/app.jsx',
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, '/public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  }
+}
