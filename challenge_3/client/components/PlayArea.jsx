98dad2f95921651aa491350512d508aa0af95857
kyruski <jamescboyett@gmail.com>
2019-03-12 15:51:55 -0500

    Adds messages when an incorrect item is chosen

--- challenge_3/client/components/PlayArea.jsx
+++ challenge_3/client/components/PlayArea.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ScoreCard from "./ScoreCard";
 import KeyPad from "./KeyPad";
 
@@ -6,6 +6,9 @@ const PlayArea = function(props) {
   let [players, setPlayers] = useState(0);
   let [scoreCard, setScoreCard] = useState(0);
   let [currIndex, setCurrIndex] = useState(0);
+  let [message, setMessage] = useState(
+    "Click the key that corresponds to the number of pins knocked down last bowl. If your second bowl was a spare, hit 'Spare'."
+  );
   let [scores, setScores] = useState([
     null,
     null,
@@ -49,30 +52,26 @@ const PlayArea = function(props) {
 
   const onKeyPadPress = selection => {
     if (selection === "Strike") {
-      if (currIndex % 2 === 0) {
-        insertIntoScore(30, 0);
-      } else {
-        console.log("Invalid Score: Cannot have a Strike as the Second Bowl");
-      }
+      currIndex % 2 === 0
+        ? insertIntoScore(30, 0)
+        : setMessage("Invalid Score: Cannot have a Strike as the Second Bowl");
     } else if (selection === "Spare") {
-      if (currIndex % 2 === 1) {
-        insertIntoScore(10);
-      } else {
-        console.log("Invalid Score: Cannot have a Spare as the First Bowl");
-      }
+      currIndex % 2 === 1
+        ? insertIntoScore(10)
+        : setMessage("Invalid Score: Cannot have a Spare as the First Bowl");
     } else {
-      if (
-        currIndex % 2 === 1 &&
-        Number(selection) + Number(scores[currIndex - 1]) > 9
-      ) {
-        console.log(
-          "Invalid Score: Cannot have a score above 9 for the Second Bowl without a Spare"
-        );
-      } else {
-        insertIntoScore(selection);
-      }
+      currIndex % 2 === 1 &&
+      Number(selection) + Number(scores[currIndex - 1]) > 9
+        ? setMessage(
+            "Invalid Score: Cannot have a score above 9 for the Second Bowl without a Spare"
+          )
+        : insertIntoScore(selection);
     }
   };
+
+  useEffect(() => {
+    setMessage("");
+  }, [scores]);
 
   return (
     <div>
@@ -80,9 +79,9 @@ const PlayArea = function(props) {
         <ScoreCard scores={scores} />
       </div>
       <div>
-        Click the key that corresponds to the number of pins knocked down last
-        bowl (not turn). If your second bowl was a spare, hit 'Spare'.
+        <br />
         <KeyPad onKeyPadPress={onKeyPadPress} />
+        <div id="message"> {message} </div>
       </div>
     </div>
   );
