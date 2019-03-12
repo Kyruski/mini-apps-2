7d70fd9c8a061ba3c01f208a749eb57548e6d875
kyruski <jamescboyett@gmail.com>
2019-03-12 10:19:19 -0500

    Adds the React skeleton

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
+const App = function() {
+  return <div>Hello All</div>;
+};
 
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bluebird = require('bluebird');
 
 const app = express();
+bluebird.promisifyAll(app);
 const port = 3000;
 
 app.use(express.json());
